b657d2c21a41fdc173cf8ef815edd404a59e3f7b
Teravla <75727791+Teravla@users.noreply.github.com>
2022-11-07 17:01:36 +0100

    update du 7/11/2022 17:01

--- main_projet_tetris.py
+++ main_projet_tetris.py
@@ -2,4 +2,30 @@
 Programme Tetris L1 BN EFREI
 '''
 
-ceci est un test
+class Grille:
+    
+    def __init__(self) -> None:
+        self.nb_colonne = 21
+
+    def grille(self):
+
+        self.grille_principale = []
+        self.grille_secondaire = []
+
+        for i in range(self.nb_colonne):
+            for j in range(self.nb_colonne):
+                self.grille_secondaire.append(0)
+            self.grille_principale.append(self.grille_secondaire)
+
+    def affichage_grille(self):
+        for k in range(len(self.grille_principale)):
+            for l in range(len(self.grille_principale[k])):
+                programme.write(str(self.grille_principale[k][l]))
+            programme.write("\n")
+
+if __name__ == "__main__":
+    gg = Grille()
+
+    gg.grille()
+    gg.affichage_grille()
+    
