9264e1eb44a270725b25b22045c857e8fe1b26b6
Teravla <75727791+Teravla@users.noreply.github.com>
2022-11-08 09:00:38 +0100

    8/11 9:00

--- main_projet_tetris.py
+++ main_projet_tetris.py
@@ -2,45 +2,98 @@
 Programme Tetris L1 BN EFREI
 '''
 
+
+path = input("Spécifier le chemin d'accès : ")
+
+
+programme = open(path, "w")
+#"C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\programme.txt"
+
+
+
 class Grille:
     
     def __init__(self) -> None:
         self.nb_colonne = 21
+        self.grille = []
+        self.lst_lettre_maj = []
+        self.lst_lettre_min = []
 
-    def grille(self):
-
-        self.grille_principale = []
-        self.grille_secondaire = []
-
+    def create_grid(self):
+        grille_2 = []
         for i in range(self.nb_colonne):
             for j in range(self.nb_colonne):
-                self.grille_secondaire.append(0)
-            self.grille_principale.append(self.grille_secondaire)
-
-    def affichage_grille(self):
-        for k in range(len(self.grille_principale)):
-            for l in range(len(self.grille_principale[k])):
-                programme.write(str(self.grille_principale[k][l]))
+                grille_2.append(0)
+            self.grille.append(grille_2)
+            grille_2 = []
+        return self.grille
+    
+    def read_grid(self):
+        programme.write('  ')
+        for k in range(len(self.lst_lettre_min)):
+            programme.write(self.lst_lettre_min[k])
+        programme.write('\n')
+        for i in range(21):
+            programme.write(self.lst_lettre_maj[i])
+            programme.write(' ')
+            for j in range(len(self.grille[i])):
+                programme.write(str(self.grille[i][j]))
             programme.write("\n")
-            
-print("Commmencer à jouer")
-print("Afficher les réglages du jeu")
+
+    
+    #Affichage des lettres
+    def lettre_min(self):
+        for i in range(97, 118):
+            lettre_min = chr(i)
+            self.lst_lettre_min.append(lettre_min)
+        #print(self.lst_lettre_min, len(self.lst_lettre_min))
+
+    def lettre_maj(self):
+        for i in range(65, 86):
+            lettre_maj = chr(i)
+            self.lst_lettre_maj.append(lettre_maj)
+        #print(self.lst_lettre_maj, len(self.lst_lettre_maj))
+    #Fin Affichage des Lettres
+    
+
+
+
+
+
+
+'''
+class Figures:
+
+    def __init__(self) -> None:
+        self.a = 5
+    
+    #def figures_cercles(self):
+
+class Regle_du_jeu:
+    def __init__(self) -> None:
+        pass           
+    
+    def affichage(self):
+        print("Commmencer à jouer")
+        print("Afficher les réglages du jeu")
+'''
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     gg = Grille()
+    gg.lettre_min()
+    gg.lettre_maj()
 
-    gg.grille()
-    gg.affichage_grille()
+
+    gg.create_grid()
+    gg.read_grid()
 
     
-    
 
     
-"""
-/!\IMPORTANT/!\
-
-Quand tu vas dans projet, tu peux peux cliquer sur les sujet (par exemple cliquer sur "grille" pour voir la description et tu peux faire glisser les slides
-pour les changer de colonnes
-
-/!\IMPORTANT/!\
-"""
