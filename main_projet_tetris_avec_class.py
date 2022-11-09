1ea4638fbd2dd7337d7d4bc6d8b51aeac3781d54
Teravla <75727791+Teravla@users.noreply.github.com>
2022-11-09 08:59:52 +0100

    9/11 8:58 -- Politique de suggestion
    Ajout de la politique de suggestion : 
    - Tous les blocs de la figure choisi
    - 3 blocs choisi aléatoirement

--- main_projet_tetris_avec_class.py
+++ main_projet_tetris_avec_class.py
@@ -1,13 +1,16 @@
+import random as rd
+
 '''
 Programme Tetris L1 BN EFREI
 '''
 
 
-#path = input("Spécifier le chemin d'accès : ")
-
+#path = input("Spécifier le chemin d'accès à la figure: ")
 
 programme = open("C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\programme.txt", "w")
 #"C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\programme.txt"
+
+
 
 
 
@@ -169,6 +172,8 @@ class Forme:
         self.forme_tous_ensemble.append(self.forme_tous_19)
         self.forme_tous_ensemble.append(self.forme_tous_20)
 
+        return self.forme_tous_ensemble
+
     
     def formes_triangle(self):
         self.forme_triangle_ensemble = []
@@ -195,6 +200,8 @@ class Forme:
         self.forme_triangle_ensemble.append(self.forme_triangle_10)
         self.forme_triangle_ensemble.append(self.forme_triangle_11)
 
+        return self.forme_triangle_ensemble
+
     
     def formes_losange(self):
         self.forme_losange_ensemble = []
@@ -205,7 +212,7 @@ class Forme:
         self.forme_losange_5= [[0,0,0,0,0],[1,0,0,1,0],[0,1,1,0,0],[0,1,1,0,0],[1,0,0,1,0]]
         self.forme_losange_6= [[0,0,0,0,0],[1,1,1,1,0],[1,1,1,1,0],[1,1,1,1,0],[1,1,1,1,0]]
         self.forme_losange_7= [[1,0,0,0,0],[1,1,0,0,0],[0,1,1,0,0],[0,0,1,1,0],[0,0,0,1,1]]
-        self.forme_losange_8= [[0,0,0,0,1],[0,0,0,1,1],[0,0,1,1,0],[0,1,1,0,0],[1,0,0,0,0]]
+        self.forme_losange_8= [[0,0,0,0,1],[0,0,0,1,1],[0,0,1,1,0],[0,1,1,0,0],[1,1,0,0,0]]
         self.forme_losange_9= [[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0]]
         self.forme_losange_10=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,1,1]]
         self.forme_losange_11=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,1,1,0],[0,0,0,1,0]]
@@ -226,6 +233,8 @@ class Forme:
         self.forme_losange_ensemble.append(self.forme_losange_12)
         self.forme_losange_ensemble.append(self.forme_losange_13)
         self.forme_losange_ensemble.append(self.forme_losange_14)
+
+        return self.forme_losange_ensemble
     
     def formes_cercle(self):
         self.forme_cercle_ensemble = []
@@ -254,12 +263,28 @@ class Forme:
         self.forme_cercle_ensemble.append(self.forme_cercle_11)
         self.forme_cercle_ensemble.append(self.forme_cercle_12)
 
-        
-        
+        return self.forme_cercle_ensemble
 
-    def test_erreur_figure(self):
-        test_affichage_figure = open("C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\test_figure.txt","w")
-        figure = self.forme_triangle_ensemble
+        
+    def path_figure(self):
+        self.path_figure = input("Spécifier le chemin d'accès aux figures possible lors de votre jeu : ")
+
+    def affichage_figure_write(self, figure):
+        #"C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\test_figure.txt"
+        test_affichage_figure = open(self.path_figure,"w")
+        for i in range(len(figure)):
+            for j in range(len(figure[i])):
+                for k in range(len(figure[i][j])):
+                    if figure[i][j][k]%2==1:
+                        test_affichage_figure.write("- ")
+                    elif figure[i][j][k]%2==0:
+                        test_affichage_figure.write("0 ")
+                test_affichage_figure.write('\n')
+            test_affichage_figure.write('\n')
+
+    def affichage_figure_append(self, figure):
+        #"C:\\Users\\User\\Documents\\Document\\EFREI\\L1\\Info\\Projet\\test_figure.txt"
+        test_affichage_figure = open(self.path_figure,"a")
         for i in range(len(figure)):
             for j in range(len(figure[i])):
                 for k in range(len(figure[i][j])):
@@ -272,10 +297,59 @@ class Forme:
 
 
 
+class Placement_figure:
+    def __init__(self) -> None:
+        Forme.__init__(self)
+
+    def blocs_alea(self,figure_1,figure_2,nb_blocs_afficher):
+        self.lst_alea=[]
+        self.lst_affichage_forme=[]
+
+        for i in range(len(figure_1)):
+            self.lst_alea.append(figure_1[i])
+        for i in range(len(figure_2)):
+            self.lst_alea.append(figure_2[i])
+
+        #CONDITION A CHANGER#
+        for i in range(1):
+
+            print("\nVoici les choix que vous avez pour 'Affichage de 3 blocs aléatoire' : \n")
+
+            for i in range(nb_blocs_afficher):
+                #Création du nombre aléatoire
+                nb_alea = rd.randint(0,len(self.lst_alea)-1)
+                
+                self.lst_affichage_forme.append(self.lst_alea[nb_alea])
+                #Suppression pour éviter la redondance
+                del self.lst_alea[nb_alea]
+                
+                #Affichage de la forme
+                for i in range(len(self.lst_affichage_forme)):
+                    for j in range(len(self.lst_affichage_forme[i])):
+                        for k in range(len(self.lst_affichage_forme[i][j])):
+                            if self.lst_affichage_forme[i][j][k]%2==1:
+                                print("-",end=" ")
+                            elif self.lst_affichage_forme[i][j][k]%2==0:
+                                print("0",end=" ")
+                        print('\n')
+                print('\n')
+                del self.lst_affichage_forme[i]
+            print('\n')
+    
+    def blocs_tous(self,figure_1,figure_2):
+        Forme.path_figure(self)
+        Forme.affichage_figure_write(self,figure_1)
+        Forme.affichage_figure_append(self,figure_2)
+        print("\nLes figures possibles sont dans le fichier que vous avez spécifiez\n")
+
+    
+
 
 class Regle_du_jeu:
     def __init__(self) -> None:
         Grille.__init__(self)
+        Forme.__init__(self)
+        Placement_figure.__init__(self)
         self.nb_colonne = 0
     
     def affichage(self):
@@ -286,13 +360,13 @@ class Regle_du_jeu:
             self.choice_user = int(input("Faire votre choix (1, 2) : "))
 
         if self.choice_user == 1:
-
-            self.nb_colonne = int(input("Donner le taille de votre tableau (21, 23 ou 25) : "))
+        
+            self.nb_colonne = int(input("\nDonner le taille de votre tableau (21, 23 ou 25) : "))
             while (self.nb_colonne!=21 and self.nb_colonne!=23) and self.nb_colonne!=25:
                 self.nb_colonne = int(input("Donner le taille de votre tableau (21, 23 ou 25) : "))
         
 
-            print("Choississez votre plateau de jeu :")
+            print("\nChoississez votre plateau de jeu :")
             print("1 - Triangle")
             print("2 - Losange")
             print("3 - Cercle")
@@ -306,31 +380,45 @@ class Regle_du_jeu:
                 Grille.lettre_maj(self)
                 Grille.figure_triangle(self)
                 Grille.read_grid(self)
+                self.politique_suggestion_blocs(Forme.formes_triangle(self),Forme.formes_tous(self))
+
             if self.choice_user_plateau == 2: #Losange
                 Grille.create_grid(self, ' ', self.nb_colonne)
                 Grille.lettre_min(self)
                 Grille.lettre_maj(self)
                 Grille.figure_losange(self)
                 Grille.read_grid(self)
+                self.politique_suggestion_blocs(Forme.formes_losange(self),Forme.formes_tous(self))
+
             if self.choice_user_plateau == 3: #Cercle
                 Grille.create_grid(self, 1, self.nb_colonne)
                 Grille.lettre_min(self)
                 Grille.lettre_maj(self)
                 Grille.figure_cercle(self, ' ')
                 Grille.read_grid(self)
+                self.politique_suggestion_blocs(Forme.formes_cercle(self),Forme.formes_tous(self))
         
+    def politique_suggestion_blocs(self,figure_1_suggestion,figure_2_suggestion):
+        print("\nVous avez le choix entre deux suggéstion de blocs :")
+        print("1 - Affichage de l'ensemble des blocks du jeu")
+        print("2 - Affichage de 3 blocs aléatoires")
+        self.choice_user_politique_suggestion = int(input("Faite votre choix : "))
+        while self.choice_user_politique_suggestion<1 or self.choice_user_politique_suggestion>2:
+            self.choice_user_politique_suggestion = int(input("Faite votre choix : "))
+        
+        if self.choice_user_politique_suggestion == 1:
+            Placement_figure.blocs_tous(self,figure_1_suggestion, figure_2_suggestion)
+        if self.choice_user_politique_suggestion == 2:
+            #Changer le 3 par un autre nombre pour avoir plus de blocs choisis aléatoirement
+            Placement_figure.blocs_alea(self,figure_1_suggestion,figure_2_suggestion,3)
+
+
         if self.choice_user == 2: 
             #Mettre les règle ici
             print()
 
 
 if __name__ == "__main__":
-    #Regle_du_jeu().affichage()
+    Regle_du_jeu().affichage()
 
-    ff = Forme()
-    ff.formes_tous()
-    ff.formes_triangle()
-    ff.formes_losange()
-    ff.formes_cercle()
-    ff.test_erreur_figure()
     
